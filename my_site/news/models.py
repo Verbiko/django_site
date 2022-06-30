367816f2762458761e26a8e1f8e45fa5829a3b07
OlgaVerbitskaya <olikverbik@mail.ru>
2022-06-30 09:22:58 +0300

    admin

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField('Заголовок', max_length=50)
+    content = models.TextField('Описание', blank=True)
+    created = models.DateTimeField('Дата создания', auto_now_add=True)
+    update = models.DateTimeField('Дата изменения', auto_now=True)
+    photo = models.ImageField('Фото', upload_to='photo/%y/%m/%d/', blank=True)
